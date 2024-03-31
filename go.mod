ce01f46b56579dc6fab8ebb4c67d25dd24634766
Carl Tablante <ctablante@seek.com.au>
2024-03-31 17:56:05 +1100

    feat: Added getElementById func

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/carlqt/anime-downloader
 
 go 1.22.0
+
+require golang.org/x/net v0.22.0
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/net/html"
 )
 
 const host = "https://subsplease.org/shows"
@@ -26,11 +27,34 @@ func main() {
 
 	defer resp.Body.Close()
 
-	bytes, _ := io.ReadAll(resp.Body)
-	fmt.Println("HTML:\n\n", string(bytes))
+	// bytes, _ := io.ReadAll(resp.Body)
+	// fmt.Println("HTML:\n\n", string(bytes))
+
+	// sid := func()
 
 	// Parse and get table element with id "show-release-table"
 	// Get the value of the attribute "sid"
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		slog.Println("Cannot parse HTML")
+		return
+	}
+
+	sid := getSID(doc)
+
+	fmt.Println("sid: ", sid)
 
 	// Then make another request to https://subsplease.org/api/?f=show&tz=Australia/Sydney&sid=%s
+}
+
+func getSID(n *html.Node) string {
+	var sid string
+	node := getElementById(n, "show-release-table")
+
+	sid, ok := GetAttribute(node, "sid")
+	if !ok {
+		log.Println("Cannot find sid")
+	}
+
+	return sid
 }
